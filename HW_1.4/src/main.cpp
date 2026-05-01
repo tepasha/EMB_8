6ac4461ec00304136b7e87223acf57097887e98e
Pavlo <tepasha.90@gmail.com>
2026-05-01 19:37:20 +0300

    add serial log for home work 1.4

--- HW_1.4/src/main.cpp
+++ HW_1.4/src/main.cpp
@@ -7,7 +7,7 @@ const char BUTTON_EXT_PIN = 4;  // External button
 const char BUTTON_BOOT_PIN = 0; // Built-in BOOT button
 
 // Mode parameters
-int blinkDelay = 500; // Initial delay (ms)
+unsigned int blinkDelay = 500; // Initial delay (ms)
 
 void setup()
 {
@@ -18,6 +18,9 @@ void setup()
   // Configure inputs for buttons
   pinMode(BUTTON_EXT_PIN, INPUT_PULLUP);
   pinMode(BUTTON_BOOT_PIN, INPUT_PULLUP);
+
+  // Add serial logger
+  Serial.begin(115200);
 }
 
 void loop()
@@ -29,6 +32,7 @@ void loop()
     if (digitalRead(BUTTON_EXT_PIN) == LOW)
     {
       blinkDelay = 100; // Fast
+      Serial.println("[EVENT] External Button Pressed: Switching to FAST (100ms)");
     }
   }
 
@@ -39,6 +43,7 @@ void loop()
     if (digitalRead(BUTTON_BOOT_PIN) == LOW)
     {
       blinkDelay = 1000; // Slow
+      Serial.println("[EVENT] BOOT Button Pressed: Switching to SLOW (1000ms)");
     }
   }
 
